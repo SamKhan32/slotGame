99664b046ebc18861eda47b4bb1d0ea5f34809fa
SamKhan32 <106261217+SamKhan32@users.noreply.github.com>
2024-05-22 02:11:29 -0400

    Update main.py

--- slotGame/main.py
+++ slotGame/main.py
@@ -4,39 +4,40 @@ from tkinter import ttk
 
 
 def spin(*args):
-    print("button pressed")
+    print("spin")
+def onSpinClick(event):
+    spinButton.config(image=spinPressed)
 
-def main():
-    
-    root = Tk()
-    root.title("Snake's Slots")
-    """PhotoImages for labels"""
-    slotMachineImage = PhotoImage(file="slotGame//res/slotmachine.png")
-    cherryImage = PhotoImage(file="slotGame//res/cherry.png")
-    cherryImage = PhotoImage(file="slotGame//res/seven.png")
-    barImage = PhotoImage(file="slotGame//res/bar.png")
-    spinUnpressed = PhotoImage(file="slotGame\\res\spinUnpressed.png")
-    """Widget Creation"""
-    frame  = ttk.Frame(root,height=500,width=1500,padding="0.5i") 
-    slotMachine = ttk.Label(frame, image=slotMachineImage)
-    slot1 = ttk.Label(slotMachine, image = barImage)
-    slot2 = ttk.Label(slotMachine, image = barImage)
-    slot3 = ttk.Label(slotMachine, image = barImage)
-    spinButton = ttk.Button(frame, image = spinUnpressed,command="spin")
-    
-    """Set Grid layout here"""
-    frame.grid(column=0,row=0,sticky="N")
-    slotMachine.grid(column=0,row=0,sticky="N")
-    slot1.grid(column=0,row=0,sticky="W")
-    slot2.grid(column=1,row=0,sticky="W")
-    slot3.grid(column=2,row=0,sticky="W")
-    spinButton.grid(column=1,row=0,sticky="W")
-    """make the padding for loop"""
-    children = frame.winfo_children()
-    for child in children:
-        child.grid_configure(padx=10,pady=10)
-    root.mainloop()
-main()
-
-
-
+def onSpinRelease(event):
+    spinButton.config(image=spinUnpressed)
+root = Tk()
+root.title("Snake's Slots")
+"""PhotoImages for labels"""
+slotMachineImage = PhotoImage(file="slotGame//res/slotmachine.png")
+cherryImage = PhotoImage(file="slotGame//res/cherry.png")
+cherryImage = PhotoImage(file="slotGame//res/seven.png")
+barImage = PhotoImage(file="slotGame//res/bar.png")
+spinUnpressed = PhotoImage(file="slotGame\\res\spinUnpressed.png")
+spinPressed = PhotoImage(file="slotGame\\res\spinPressed.png")
+"""Widget Creation"""
+frame  = ttk.Frame(root,height=500,width=1500,padding="0.5i") 
+slotMachine = ttk.Label(frame, image = slotMachineImage)
+slot1 = ttk.Label(slotMachine, image = barImage)
+slot2 = ttk.Label(slotMachine, image = barImage)
+slot3 = ttk.Label(slotMachine, image = barImage)
+spinButton = ttk.Button(frame, image = spinUnpressed,command=spin)
+"""Set Grid layout"""
+frame.grid(column=0,row=0,sticky="N")
+slotMachine.grid(column=0,row=0,sticky="N")
+slot1.grid(column=0,row=0,sticky="W")
+slot2.grid(column=1,row=0,sticky="W")
+slot3.grid(column=2,row=0,sticky="W")
+spinButton.grid(column=1,row=0,sticky="W")
+"""Bind Button Function"""
+spinButton.bind("<ButtonPress-1>", onSpinClick)
+spinButton.bind("<ButtonRelease-1>", onSpinRelease)
+"""make the padding for loop"""
+children = frame.winfo_children()
+for child in children:
+    child.grid_configure(padx=10,pady=10)
+root.mainloop()
