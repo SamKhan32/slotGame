0f63560fd45bb9eb257699f31d144a44f1ae5189
SamKhan32 <106261217+SamKhan32@users.noreply.github.com>
2024-05-23 22:26:15 -0400

    Update pane.py

--- slotGame/pane.py
+++ slotGame/pane.py
@@ -1,12 +1,1 @@
-from tkinter import *
-from tkinter import ttk
-cherryPath ="slotGame\\res\cherry.png"
-sevenPath = "slotGame\\res\seven.png"
-barPath = "slotGame\\res\\bar.png"
-class pane(object):
-    """A window is an object that contains an image represented by a string"""
-    image = "bar"
-    
-    def setImage(self,image):
-        self.image = image        
-    
+
