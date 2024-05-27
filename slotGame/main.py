22f0f32e29667b7b245f4ec029474415be66de4c
SamKhan32 <106261217+SamKhan32@users.noreply.github.com>
2024-05-26 22:09:27 -0400

    Update main.py

--- slotGame/main.py
+++ slotGame/main.py
@@ -2,38 +2,57 @@ from tkinter import *
 from tkinter import ttk
 from Pane import Pane
 import time
+from threading import Thread
+import random as r
 
+def updateImages(pane, slot):
+    if(pane.getImage() == "cherry"):
+        print("cherry")
 
-def spin(*args):
+def spinning(label):
+    for image in images:
+        label.config(image = image)
+        root.update()
+def gameAction(*args):
+    print("Spin method reached")
+    pane1.generateSlot()
+    pane2.generateSlot()
+    pane3.generateSlot()
     target = (time.time() * 1000 ) + 2500
-    
     while (time.time()*1000) < target:
-        slot1.config(image = sevenImage)
-        root.update()
-        slot1.config(image = cherryImage)
-        root.update()
-        slot1.config(image = barImage)
-        root.update()
-    slot1.config(image=cherryImage)
-    
-    
-
+        spinning(slot1)
+        spinning(slot2)
+        spinning(slot3)
+    updateImages(pane1)
+    updateImages(pane2)
+    updateImages(pane3)
+    spinButton.config(state=NORMAL)
 def onSpinClick(event):
-    spinButton.config(image=spinPressed)
-
+    print("Spin Button Click Reach")
+    print(spinButton['state'])
+    if spinButton['state'] == "normal":
+        print("If statement reached")
+        spinButton.config(state=DISABLED)
+        spinButton.config(image=spinPressed)
+        gameAction()
 def onSpinRelease(event):
     spinButton.config(image=spinUnpressed)
 root = Tk()
 root.title("Snake's Slots")
+"""Symbol Weights"""
+cherryWeight = 50
+sevenWeight = 30
+barWeight = 20
 """Pane Creation"""
 pane1 = Pane()
 pane2 = Pane()
 pane3 = Pane()
 """PhotoImages for labels"""
 slotMachineImage = PhotoImage(file="slotGame//res/slotmachine.png")
+sevenImage = PhotoImage(file="slotGame//res/seven.png")
 cherryImage = PhotoImage(file="slotGame//res/cherry.png")
-cherryImage = PhotoImage(file="slotGame//res/seven.png")
 barImage = PhotoImage(file="slotGame//res/bar.png")
+images = (sevenImage,cherryImage,barImage)
 spinUnpressed = PhotoImage(file="slotGame\\res\spinUnpressed.png")
 spinPressed = PhotoImage(file="slotGame\\res\spinPressed.png")
 """Widget Creation"""
@@ -42,7 +61,7 @@ slotMachine = ttk.Label(frame, image = slotMachineImage)
 slot1 = ttk.Label(slotMachine, image = barImage)
 slot2 = ttk.Label(slotMachine, image = barImage)
 slot3 = ttk.Label(slotMachine, image = barImage)
-spinButton = ttk.Button(frame, image = spinUnpressed,command=spin)
+spinButton = ttk.Button(frame, image = spinUnpressed)
 """Set Grid layout"""
 frame.grid(column=0,row=0,sticky="N")
 slotMachine.grid(column=0,row=0,sticky="N")
@@ -58,6 +77,9 @@ children = frame.winfo_children()
 for child in children:
     child.grid_configure(padx=10,pady=10)
 root.mainloop()
+
+
+
 
 
 
